a9f3a7035262484d64287f8fdff6363d758bf6bb
dmontenegroh <dmontenegro.top@gmail.com>
2023-09-04 16:59:55 -0300

    Dia 12

--- Dia_12/mi_restaurante.py
+++ Dia_12/mi_restaurante.py
@@ -1,10 +1,39 @@
 from tkinter import *
 
+operador = ''
+
+
+def click_boton(numero):
+    global operador
+    operador = operador + numero
+    visor_calculadora.delete(0, END)
+    visor_calculadora.insert(END, operador)
+
+
+def borrar():
+    global operador
+    operador = ''
+    visor_calculadora.delete(0, END)
+
+
+def obtener_resultado():
+    global operador
+
+    resultado = str(eval(operador))
+    visor_calculadora.delete(0, END)
+    visor_calculadora.insert(0, resultado)
+    operador = ''
+
+
+def revisar_check():
+    pass
+
+
 # iniciar tkinter
 aplicacion = Tk()
 
 # tamaño de la ventana
-aplicacion.geometry('1500x630+450+100')
+aplicacion.geometry('1155x630+0+0')
 
 # evitar maximizar
 aplicacion.resizable(0, 0)
@@ -31,7 +60,7 @@ panel_izquierdo = Frame(aplicacion, bd=1, relief=FLAT, bg='#ffffff')
 panel_izquierdo.pack(side=LEFT)
 
 # panel costos
-panel_costos = Frame(aplicacion, bd=1, relief=FLAT, bg='#ffffff', padx=50)
+panel_costos = Frame(panel_izquierdo, bd=1, relief=FLAT, bg='#ffffff', padx=50)
 panel_costos.pack(side=BOTTOM)
 
 # panel comidas
@@ -53,15 +82,15 @@ panel_derecha = Frame(aplicacion, bd=1, relief=FLAT)
 panel_derecha.pack(side=RIGHT)
 
 # panel calculadora
-panel_calculadora = Frame(aplicacion, bd=1, relief=FLAT, bg='#eaf1fb')
+panel_calculadora = Frame(panel_derecha, bd=1, relief=FLAT, bg='#eaf1fb')
 panel_calculadora.pack()
 
 # panel recibo
-panel_recibo = Frame(aplicacion, bd=1, relief=FLAT, bg='#eaf1fb')
+panel_recibo = Frame(panel_derecha, bd=1, relief=FLAT, bg='#eaf1fb')
 panel_recibo.pack()
 
 # panel botones
-panel_botones = Frame(aplicacion, bd=1, relief=FLAT, bg='#eaf1fb')
+panel_botones = Frame(panel_derecha, bd=1, relief=FLAT, bg='#eaf1fb')
 panel_botones.pack()
 
 lista_comidas = ['pollo', 'cordero', 'salmon',
@@ -85,7 +114,8 @@ for comida in lista_comidas:
                          text=comida.title(),
                          font=('Dosis', 19, 'bold'),
                          onvalue=1,
-                         offvalue=0, bg='#ffffff', variable=variables_comida[contador])
+                         offvalue=0, bg='#ffffff', variable=variables_comida[contador],
+                         command=revisar_check)
 
     comida.grid(row=contador, column=0, sticky=W)
 
@@ -111,7 +141,7 @@ for bebida in lista_bebidas:
     variables_bebida.append('')
     variables_bebida[contador] = IntVar()
     bebida = Checkbutton(panel_bebidas, text=bebida.title(), font=(
-        'Dosis', 19, 'bold'), onvalue=1, offvalue=0, bg='#ffffff', variable=variables_bebida[contador])
+        'Dosis', 19, 'bold'), onvalue=1, offvalue=0, bg='#ffffff', variable=variables_bebida[contador], command=revisar_check)
 
     bebida.grid(row=contador, column=0, sticky=W)
 
@@ -134,7 +164,7 @@ for postre in lista_postres:
     variables_postre.append('')
     variables_postre[contador] = IntVar()
     postre = Checkbutton(panel_postres, text=postre.title(), font=(
-        'Dosis', 19, 'bold'), onvalue=1, offvalue=0, bg='#ffffff', variable=variables_postre[contador])
+        'Dosis', 19, 'bold'), onvalue=1, offvalue=0, bg='#ffffff', variable=variables_postre[contador], command=revisar_check)
 
     postre.grid(row=contador, column=0, sticky=W)
 
@@ -250,5 +280,91 @@ texto_total = Entry(panel_costos,
 texto_total.grid(row=2, column=3, padx=41)
 
 
+# botones
+botones = ['total', 'recibo', 'guardar', 'resetear']
+columnas = 0
+for boton in botones:
+    boton = Button(panel_botones,
+                   text=boton.title(),
+                   font=('Dosis', 14, 'bold'),
+                   fg='white',
+                   bg='azure4',
+                   bd=1,
+                   width=9)
+
+    boton.grid(row=0, column=columnas)
+    columnas += 1
+
+
+# area de recibo
+texto_recibo = Text(panel_recibo,
+                    font=('Dosis', 12, 'bold'),
+                    bd=1,
+                    width=50,
+                    height=10)
+texto_recibo.grid(row=0, column=0)
+
+# calculadora
+visor_calculadora = Entry(panel_calculadora,
+                          font=('Dosis', 16, 'bold'),
+                          width=38,
+                          bd=1)
+
+visor_calculadora.grid(row=0, column=0, columnspan=4)
+
+botones_calculadora = ['7', '8', '9', '+',
+                       '4', '5', '6', '-',
+                       '1', '2', '3', 'x',
+                       '=', 'Borrar', '0', '/',]
+
+botones_guardados = []
+
+
+fila = 1
+columna = 0
+
+for boton in botones_calculadora:
+    boton = Button(panel_calculadora,
+                   text=boton.title(),
+                   font=('Dosis', 16, 'bold'),
+                   fg='white',
+                   bg='azure4',
+                   bd=1,
+                   width=8)
+
+    botones_guardados.append(boton)
+
+    boton.grid(row=fila, column=columna)
+
+    if columna == 3:
+        fila += 1
+
+    columna += 1
+
+    if columna == 4:
+        columna = 0
+
+botones_guardados[0].config(command=lambda: click_boton('7'))
+botones_guardados[1].config(command=lambda: click_boton('8'))
+botones_guardados[2].config(command=lambda: click_boton('9'))
+botones_guardados[3].config(command=lambda: click_boton('+'))
+botones_guardados[4].config(command=lambda: click_boton('4'))
+botones_guardados[5].config(command=lambda: click_boton('5'))
+botones_guardados[6].config(command=lambda: click_boton('6'))
+botones_guardados[7].config(command=lambda: click_boton('-'))
+botones_guardados[8].config(command=lambda: click_boton('1'))
+botones_guardados[9].config(command=lambda: click_boton('2'))
+botones_guardados[10].config(command=lambda: click_boton('3'))
+botones_guardados[11].config(command=lambda: click_boton('*'))
+botones_guardados[12].config(command=obtener_resultado)
+botones_guardados[13].config(command=borrar)
+botones_guardados[14].config(command=lambda: click_boton('0'))
+botones_guardados[15].config(command=lambda: click_boton('/'))
+
+
 # evitar que la pantalla se cierre
 aplicacion.mainloop()
+
+
+# suma = 11069+39996+3000+18300+1500+11041+9658+11715+7090+6928+5128+18480+17760
+# print(suma)
